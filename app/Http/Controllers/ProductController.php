c8044d5ea21e6617ecc722686ab3811e69a0200e
albsosa <inf.albsosa@gmail.com>
2017-05-22 22:39:47 -0500

    Lista de productos
    puse los datos directos de la base de datos, no reconocía los del .env
    y ya muestra la lista de todos los productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,13 @@
 namespace crud54\Http\Controllers;
 
 use Illuminate\Http\Request;
+use crud54\product;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+    	$products = product::orderBy('id', 'DESC')->paginate();
+    	return view('products.index', compact('products'));
+
+    }
 }
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layout')
+
+
+@section('content')
+<div class="col-sm-8">
+	<h2>Lista de Productos
+		<a href="{{route('products.create')}}" class="btn btn-primary pull-right">Nuevo</a>
+	</h2>
+	<table class="table table-hover table-striped">
+		<thead>
+		<tr>
+			<th width="20px">ID</th>
+			<th>Nombre del producto</th>
+			<th colspan="3">&nbsp;</th>
+		</tr>
+		</thead>
+		<tbody>
+			@foreach($products as $product)
+			<tr>
+			<td>{!!$product->id!!}</td>
+			<td><strong>{!!$product->name!!}</strong> {!! $product->short!!}</td>
+			<td>Ver</td>
+			<td>Editar</td>
+			<td>Eliminar</td>
+			</tr>
+
+			@endforeach
+		</tbody>
+	</table>
+	<!-paginacion-->
+	{!! $products->render() !!}
+</div>
+<div class="col-sm-4">
+	Mensaje
+</div>
+
+@endsection
